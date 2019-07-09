4d094a8bf77c409363896530d9bf8b5f3dfe9e96
TodoAsap <marek_dell+todoasap@users.noreply.github.com>
2019-07-09 15:56:29 +0200

    play around with ssl libs

---
+++ CEX Library/Experiment/TcpClientWithSsl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TcpClientWithSsl
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello World!");
+        //}
+    }
+}
